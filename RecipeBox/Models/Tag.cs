3dc828897081022f42aac18061529b37a701918e
Jason Falk <jasonfalk9@gmail.com>
2024-05-02 15:51:27 -0700

    Add tag model.

---
+++ RecipeBox/Models/Tag.cs
@@ -0,0 +1,12 @@
+using System.Collection.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Tag
+  {
+    public int TagId { get; set; }
+    [Required(ErrorMessage = "Tag category can't be left empty.")]
+    public string Category { get; set; }
+    public List<RecipeTag> JoinEntities { get; }
+  }
+}
