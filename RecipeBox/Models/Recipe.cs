fab7efd6fb9d03daf11a1f9cf4d54d83fd59b002
Jason Falk <jasonfalk9@gmail.com>
2024-05-13 11:30:17 -0700

    Remove ingredient value and add list of ingredient models.

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -8,11 +8,10 @@ namespace RecipeBox.Models
     public int RecipeId { get; set; }
     [Required(ErrorMessage = "The recipe name can't be empty.")]
     public string RecipeName { get; set; }
-    [Required(ErrorMessage = "The recipe ingredients can't be empty.")]
-    public string Ingredients { get; set; }
     [Required(ErrorMessage = "The recipe instructions can't be empty.")]
     public string RecipeInstructions  { get; set; }
     public int RecipeRating { get; set; } = 0;
+    public List<Ingredient> Ingredients { get; set; }
     public List<RecipeTag> JoinEntities { get; }
     public ApplicationUser User { get; set; }
   }
